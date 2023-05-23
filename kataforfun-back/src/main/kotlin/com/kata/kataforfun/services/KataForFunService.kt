10ef6e337544c15d56596a1f57f26cabea66c197
HAMROUNI Ilyes <ihamrouni@devoteam.com>
2023-05-23 11:29:44 +0200

    created kataforfunservice functions with tests passed

--- kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.kt
+++ kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.kt
@@ -5,8 +5,44 @@ import org.springframework.stereotype.Component
 @Component
 class KataForFunService {
 
-    fun convertNumber(inputNumber: Int): String {
-        return ""
+    val dict = mapOf(
+        "3" to "Kata",
+        "5" to "For",
+        "7" to "Fun"
+    )
+
+    fun convertNumber(number: Int): String {
+        var word = ""
+        if (divisible(number, 3)) {
+            word += "Kata"
+        }
+        if (divisible(number, 5)) {
+            word += "For"
+        }
+        word += convertContains(number.toString(), dict)
+
+        if (word.isEmpty()) {
+            return number.toString()
+        }
+        return word
+    }
+
+    /**
+     * takes input number and replaces each key number occurence with a string vlaue
+     */
+    fun convertContains(number: String, dict: Map<String, String>): String {
+        var word = ""
+        for (index in number.indices) {
+            if (dict[number[index].toString()] != null) {
+                word += dict[number[index].toString()]!!
+            }
+        }
+        return word
+    }
+
+
+    fun divisible(number: Int, divisor: Int): Boolean {
+        return number % divisor == 0
     }
 
 }
